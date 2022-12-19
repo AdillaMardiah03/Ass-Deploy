3a04343f7f49eaf2c64871cff220020175527670
AdillaMardiah03 <adillamardiah32@gmail.com>
2022-12-19 21:16:52 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Adilla Mardiah" // TODO: replace this
+const studentId = "FE3006319" // TODO: replace this
+
+export { url, studentName, studentId }
